855ec909384752f3673fb5e9f2ed7019341352db
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-06 15:21:45 +0200

    Uppgift 1 klart

--- UPG1/Program.cs
+++ UPG1/Program.cs
@@ -6,7 +6,18 @@ namespace UPG1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Skriv in en mening:");
+
+            string mening = Console.ReadLine();
+            string[] meningArray = mening.Split(' ');   // Delar upp orden i meningen och sätter in dom i en Array, ' ' är divider
+            Console.WriteLine("Din mening innehåller: " + meningArray.Length + " ord");     //Skriver ut antal ord som är lagrat i Arrayebn
+            for (int i = 0; i < meningArray.Length; i++)    //För varje ord loopas for-iterationen och skriver ut antal bokstäver
+            {
+
+                Console.WriteLine(meningArray[i] + " har " + meningArray[i].Length + " bokstäver!");
+            }
+
+            Console.ReadLine();
         }
     }
 }
