6539f90f55f622f5e33a487ffb8553d82b581638
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-06 14:41:41 +0200

    Tomma uppgifter

---
+++ UPG2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UPG2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UPG3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UPG3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
