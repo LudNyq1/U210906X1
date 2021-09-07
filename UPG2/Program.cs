28c9a5f2655732f1d649a3b417834db24b300686
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-07 13:37:13 +0200

    For sats för stigande ordning klar.

--- UPG2/Program.cs
+++ UPG2/Program.cs
@@ -8,9 +8,10 @@ namespace UPG2
         {
             Console.WriteLine("Skriv in din mening:");
             string mening = Console.ReadLine();
-            string[] ord = mening.Split(' '); //Skapa array av s, innehåller ord för ord
+            string[] ord = mening.Split(' '); //Skapa array av mening innehållande ord för ord
 
-            //For satsen nedan sorterar listan i fallande ordning (Störst till minst)
+            //For satsen nedan sorterar ord-listan i fallande ordning (Störst till minst)
+            Console.WriteLine("Orden i fallande ordning: ");
             for (int i = 0; i < ord.Length - 1; i++) 
             {
                 if (ord[i].Length < ord[i + 1].Length)
@@ -27,7 +28,25 @@ namespace UPG2
                 Console.WriteLine(i);
             }
 
+            //For satsen nedan sorterar ord-listan i stigande ordning (Minst till störst)
+            Console.WriteLine("Orden i stigande orden: ");
+            for (int i = 0; i < ord.Length - 1; i++)
+            {
+                if (ord[i].Length > ord[i + 1].Length)
+                {
+                    string temp = ord[i];
+                    ord[i] = ord[i + 1];
+                    ord[i + 1] = temp;
 
+                    i = -1;
+                }
+            }
+            foreach (string i in ord)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
 
         }
     }
