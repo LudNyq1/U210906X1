cfb2deacc01e3817170858d730f6057955c345d2
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-07 11:55:32 +0200

    Tog bort for loop sorteringen, kunde ej lösa

--- UPG2/Program.cs
+++ UPG2/Program.cs
@@ -7,8 +7,24 @@ namespace UPG2
         static void Main(string[] args)
         {
             Console.WriteLine("Skriv in din mening:");
-            string s = Console.ReadLine();
-            string[] sArr = s.Split(' '); //Skapa array av s, innehåller ord för ord
+            string mening = Console.ReadLine();
+            string[] meningLista = mening.Split(' '); //Skapa array av s, innehåller ord för ord
+
+
+
+            /*for (int i = 0; i < meningLista.Length - 1; i++) //Sotera lista, fallande ordning
+            {
+                if (meningLista[i].Length < meningLista[i + 1].Length)
+                {
+                    string temp = meningLista[i];
+                    meningLista[i] = meningLista[i + 1];
+                    meningLista[i + 1] = temp;
+                }
+            }
+            foreach (string ord in meningLista)
+            {
+                Console.WriteLine(ord);
+            }*/
         }
     }
 }
