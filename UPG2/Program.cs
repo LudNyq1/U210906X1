0a8fe547b78be4cdb575202995f4fcbea620d075
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-07 12:19:23 +0200

    Jag vet inte hur jag ska klara uppgiften med att sortera arrayen utan Array.sort()!

--- UPG2/Program.cs
+++ UPG2/Program.cs
@@ -11,8 +11,9 @@ namespace UPG2
             string[] meningLista = mening.Split(' '); //Skapa array av s, innehåller ord för ord
 
 
-
-            /*for (int i = 0; i < meningLista.Length - 1; i++) //Sotera lista, fallande ordning
+            //Jag vet inte hur jag ska klara uppgiften med att sortera arrayen utan Array.sort()!
+            //Nedan är mitt försök men det kastar bara om orden och jämför dom en och en.
+            /*for (int i = 0; i < meningLista.Length - 1; i++) 
             {
                 if (meningLista[i].Length < meningLista[i + 1].Length)
                 {
