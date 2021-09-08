0b9036500542ea3dbdc10ab9aa8212bf4c13c5f5
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-08 12:31:27 +0200

    Koonverterat stringarna till arrays

--- UPG7/Program.cs
+++ UPG7/Program.cs
@@ -9,8 +9,12 @@ namespace UPG7
             string leetSpeak = "4 8 ( |) 3 |# 6 |-| ! _) |( 1 |\\\\/| |\\\\| 0 |> ? |2 5 + |_| \\\\/ \\\\|/ % `/ 7_";
             string alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
 
-            string[] leetSpeakArray = leetSpeak.Split(' ');
-            string[] alphabetArray = alphabet.Split(' ');
+            string[] leetSpeakArray = leetSpeak.Split(' '); //Konverterar leetSpeak stringen till en array
+            string[] alphabetArray = alphabet.Split(' '); //Konverterar leetSpeak stringen till en array
+            for(int i = 0; i < alphabetArray.Lenght)
+            {
+
+            }
             Console.WriteLine(leetSpeak);
         }
     }
