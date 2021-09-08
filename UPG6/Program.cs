bcca72de0fdf71c915bcb79b2ea1e4c85f3d4669
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-08 11:54:21 +0200

    Lagt till kommentarer

--- UPG6/Program.cs
+++ UPG6/Program.cs
@@ -10,7 +10,7 @@ namespace UPG6
             int rTal = rnd.Next(1, 21);
             int gissning = 0;
             Console.WriteLine("Gissa ett tal mellan 1-21: ");
-            while (gissning != rTal)
+            while (gissning != rTal) // Checkar om gissningen är lika med randomtalet, om inte fortsätter while loopen
             {
                 gissning = int.Parse(Console.ReadLine());
                 Console.WriteLine("Talet var fel, gissa igen!");
