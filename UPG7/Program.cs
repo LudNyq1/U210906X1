79e84dd71a5ecab156704b624f8eacefee5e9fe6
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-08 18:26:49 +0200

    Kod är kommenterat och klart

--- UPG7/Program.cs
+++ UPG7/Program.cs
@@ -6,30 +6,28 @@ namespace UPG7
     {
         static void Main(string[] args)
         {
-            string alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z, ";
-            string leetSpeak = "4,8,(,|),3,|#,6,|-|,!,_),|(,1,|\\\\/|,|\\\\|,0,|>,?,|2,5,+,|_|,\\\\/,\\\\|/,%,`/,7_, ";
+            string alphabet = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z, "; //Alphabetet med mellanslag
+            string leetSpeak = "4,8,(,|),3,|#,6,|-|,!,_),|(,1,|\\\\/|,|\\\\|,0,|>,?,|2,5,+,|_|,\\\\/,\\\\|/,%,`/,7_, "; //LeetSpeak alphabetet (a-z) med mellanslag
 
             string[] alphabetArray = alphabet.Split(','); //Konverterar leetSpeak stringen till en array
             string[] leetSpeakArray = leetSpeak.Split(','); //Konverterar leetSpeak stringen till en array
 
-            /*string[,] alphabetLeet2d = new string[26, 2];
-            for(int i = 0; i < alphabetArray.Length; i++) //Foor loopen sätter in bokstäverna och dess motsvarande leetSpeak "tecken" i samma position.
-            {
-                alphabetLeet2d[i, 0] = alphabetArray[i];
-                alphabetLeet2d[i, 1] = leetSpeakArray[i];
-            }*/
-            Console.WriteLine("Skriv en mening utan å ä ö:");
+            /*ANVÄNDER EJ 2D ARRAY FÖR DET INTE BEHÖVS, MEN MAN KAN!*/
 
-            char[] meningBokstäver = Console.ReadLine().ToCharArray();
+            Console.WriteLine("Skriv en mening utan å ä ö som ska översättas till Leet Speak:");
+            char[] meningBokstäver = Console.ReadLine().ToCharArray(); //Omvandlar stringen som skriv tin till charArray
             string[] meningStringArray = new string[meningBokstäver.Length];
-            for(int i = 0; i < meningBokstäver.Length; i++)
+            for (int i = 0; i < meningBokstäver.Length; i++) //Omvandlar meningBokstäver till string[]
             {
                 meningStringArray[i] = meningBokstäver[i].ToString();
             }
 
-            string meningLeet = "";
+            string meningLeet = ""; // Skapar stringen pga enklare utskrift till Leet
 
-            for (int i = 0; i < meningStringArray.Length; i++)
+            for (int i = 0; i < meningStringArray.Length; i++) // Kollar vilka platser i alphabet
+                                                               // som motsvarar bokstäverna i meningStringArray,
+                                                               // eftersom poisition 0 motsvarar a och 4 i både alphabetet och leetSpeak
+                                                               // är listorna "paralella"
             {
                 meningLeet += leetSpeakArray[Array.IndexOf(alphabetArray, meningStringArray[i])];
             }
