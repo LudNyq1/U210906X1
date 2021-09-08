3a14dd7eb834597c7adabe81e3689d71ac1656dc
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-08 12:29:05 +0200

    Skrivit in leepSpeak alphabet och vanliga alphabet

--- UPG7/Program.cs
+++ UPG7/Program.cs
@@ -6,7 +6,12 @@ namespace UPG7
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string leetSpeak = "4 8 ( |) 3 |# 6 |-| ! _) |( 1 |\\\\/| |\\\\| 0 |> ? |2 5 + |_| \\\\/ \\\\|/ % `/ 7_";
+            string alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+
+            string[] leetSpeakArray = leetSpeak.Split(' ');
+            string[] alphabetArray = alphabet.Split(' ');
+            Console.WriteLine(leetSpeak);
         }
     }
 }
