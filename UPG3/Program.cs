6fe33163171b11d0b185c04fb1d35230feb75d83
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-09 07:15:51 +0200

    skapat for loopar som "rakar bort" tomma platser i unikaOrd(1) och skapar en lista som endast innehåller de unika orden

--- UPG3/Program.cs
+++ UPG3/Program.cs
@@ -9,20 +9,45 @@ namespace UPG3
             Console.WriteLine("Skriv in din mening:");
 
             string[] mening = Console.ReadLine().Split(' ') /*{ "jag", "heter", "ludvig", "jag", "bajs", "heter" }*/;
-            string[] unikaOrd = new string[mening.Length];
-            int ctr = 0;
+            string[] unikaOrd1 = new string[mening.Length];
+            int ctr1 = 0;
             foreach(string s in mening)
             {
                 
-                if (Array.IndexOf(unikaOrd, s) == -1) //Om ordet s inte finns i unikaOrd, genomförs ej if satsen.
+                if (Array.IndexOf(unikaOrd, s) == -1) //Om ordet s inte finns i unikaOrd, genomförs if satsen.
+                                                      //Array.IndexOf() retunerar värdet -1 om den börjar söra på pos[0] men ej hittar värdet
                 {
-                    unikaOrd[ctr] = mening[Array.IndexOf(mening, s)]; //Hittar värdet på stringen s och sätter in det i unikaOrd
-                    ctr++;
+                    unikaOrd[ctr1] = mening[Array.IndexOf(mening, s)]; //Hittar värdet på stringen s och sätter in det i unikaOrd
+                    ctr1++;
                 }
                 
             }
 
-            foreach (string i in unikaOrd)
+            int ctr2 = 0;
+            for (int i = 0; i < unikaOrd1.Length; i++) //Ser hur många platser som är fyllda i unikaOrd
+            {
+                if (unikaOrd1[i] != "")
+                {
+                    ctr2++;
+                }
+            }
+
+            string[] unikaOrd2 = new string[ctr2];
+
+            for (int i = 0; i < unikaOrd1.Length; i++) //Ser hur många platser som är fyllda i unikaOrd
+            {
+                if (unikaOrd1[i] != "") //Om unikaOrd1[i] är en tom plats genomförs ej if-satsen, orden tilldelas ej till arrayen unikaOrd2
+                                       
+                {
+                    unikaOrd2[i] = unikaOrd1[i];
+                }
+            }
+
+
+
+
+
+            foreach (string i in unikaOrd2)
             {
                 Console.WriteLine(i);
             }
