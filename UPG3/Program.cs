4e3cbb0abf4274bf684a9b309ef67b67cfa486cb
Ludvig Nyqvist <ludvig.nyqvist1@outlook.com>
2021-09-09 10:04:56 +0200

    sorteringen för unika orden och antalen de förekommer klart, i fallande ordning. UPG3 klar

--- UPG3/Program.cs
+++ UPG3/Program.cs
@@ -11,16 +11,16 @@ namespace UPG3
             string[] mening = Console.ReadLine().Split(' ') /*{ "jag", "heter", "ludvig", "jag", "bajs", "heter" }*/;
             string[] unikaOrd1 = new string[mening.Length];
             int ctr1 = 0;
-            foreach(string s in mening)
+            foreach (string s in mening)
             {
-                
+
                 if (Array.IndexOf(unikaOrd1, s) == -1) //Om ordet s inte finns i unikaOrd, genomförs if satsen.
-                                                      //Array.IndexOf() retunerar värdet -1 om den börjar söra på pos[0] men ej hittar värdet
+                                                       //Array.IndexOf() retunerar värdet -1 om den börjar söra på pos[0] men ej hittar värdet
                 {
                     unikaOrd1[ctr1] = mening[Array.IndexOf(mening, s)]; //Hittar värdet på stringen s och sätter in det i unikaOrd
                     ctr1++;
                 }
-                
+
             }
 
             int ctr2 = 0;
@@ -45,7 +45,7 @@ namespace UPG3
                 int ctr = 0;
                 for (int j = 0; j < mening.Length; j++)
                 {
-                    if(unikaOrd2[i] == mening[j])
+                    if (unikaOrd2[i] == mening[j])
                     {
                         ctr++;
                     }
@@ -53,16 +53,33 @@ namespace UPG3
                 unikaOrdAntal[i] = ctr;
             }
 
-            foreach ( item in collection)
+            Console.WriteLine("De unika orden och hur många gånger de förekommer:");
+            for (int i = 0; i < unikaOrd2.Length; i++)
             {
-
+                Console.WriteLine($"{unikaOrd2[i]} \t ({unikaOrdAntal[i]})");
             }
 
-            for(int i = 0; i < unikaOrd2.Length; i++)
+            for (int i = 0; i < unikaOrd2.Length - 1; i++) //Sorterar unikaOrdAntal och unikaOrd2 parallellt med varandra, fallande ordning.
             {
-                Console.WriteLine(unikaOrd2[i] + " " + unikaOrdAntal[i]);
+                if (unikaOrdAntal[i] > unikaOrdAntal[i + 1])
+                {
+                    int temp = unikaOrdAntal[i];
+                    unikaOrdAntal[i] = unikaOrdAntal[i + 1];
+                    unikaOrdAntal[i + 1] = temp;
+
+                    string temp1 = unikaOrd2[i];
+                    unikaOrd2[i] = unikaOrd2[i + 1];
+                    unikaOrd2[i + 1] = temp1;
+
+                    i = -1;
+                }
             }
-            
+            Console.WriteLine("De unika orden sorterat i hur många gånger de förekommer i fallande ordning:");
+            for (int i = 0; i < unikaOrd2.Length; i++)
+            {
+                Console.WriteLine($"{unikaOrd2[i]} \t ({unikaOrdAntal[i]})");
+            }
+
             /*
             string[] s = { "jag", "cool", "ludvig", "ball", "koolalkalkalkalka"};
             Console.WriteLine(Array.IndexOf(s, "jagg")); //Om objektet inte finns i arrayen returnar den det sista 
